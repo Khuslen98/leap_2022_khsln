02b36a9cb60e909f5c92e47af3183107a726bcac
Morning <morning@Lab-02-01s-iMac.local>
2022-11-28 12:36:20 +0800

    Java script

---
+++ day-10/js/main.js
@@ -0,0 +1,69 @@
+console.log
+('Hola mi madamme li mico de')
+// alert
+// ("hola from javascript migo de alerte!")
+console.log
+('Hola mi amigos de locos!!!!');
+
+var hello ;
+console.log(hello);
+
+hello = "hello world from js!!!!!"
+console.log(hello);
+
+hello = "here is now ninjava!!"
+console.log(hello);
+
+let myNumber;
+console.log(myNumber);
+
+myNumber = 5; 
+console.log(myNumber);
+
+let x = 8, y = 6;
+console.log(x + y);
+
+console.log(x + y + 12)
+
+console.log((x * x)+(y * y));
+
+console.log(x * y)
+
+const PI = 3.14;
+
+console.log(PI);
+
+// PI = 6.14; 
+// console.log (PI);
+
+var a = 6;
+var z = 8;
+// var p = +;
+console.log(a);
+
+// let b = 6; 
+// let b = 8;
+
+// console.log(b);
+
+let message = "Hi";
+console.log(message);
+
+var b = " added"
+console.log(b);
+
+// var c = " a + z ";
+// console.log(c);
+
+var sum = a + z;
+
+alert 
+(message + b  + a + z)
+
+
+var d = "It is "
+let = "a+z"
+
+alert
+(d + sum)
+
