16bb67b46f7410a3880b7d7c8de2c28a70c87838
Morning <morning@Lab-02-01s-iMac.local>
2022-11-29 12:31:25 +0800

    Javascript some exercises are done

---
+++ day-11/js/main.js
@@ -0,0 +1,196 @@
+// let a = "Baby shark "
+// let b = "Doo Doo "
+// let c = "Mommy Shark "
+
+// console.log (a + b + b)
+// console.log (a + b + b)
+// console.log (a + b + b)
+// console.log (a + b + b)
+// console.log (c + b + b)
+// console.log (c + b + b)
+// console.log (c + b + b)
+
+// let myNumber = 5; 
+// let undify;
+// let nullVariable = null;
+// let myDecision = true;
+
+// // typeof 
+// console.log(typeof 1);
+// console.log(typeof "hello");
+// console.log(typeof undify);
+// console.log(typeof nullVariable);
+// console.log(myDecision)
+// console.log(typeof myDecision);
+
+// let x = "4"
+// let y = "8"
+// let z = "10"
+
+// console.log(7%2);
+// console.log(36%19);
+// // uldegdeltei huvaah arga 
+
+// let result = '30' - 10;
+// console.log(result);
+
+// // alert((x*x)+(y*y)-(z*z*z)/240*175)
+
+// // alert((x+y)/2*z)
+
+// const PI = 3.14;
+// let r = 45;
+
+// let circleLength = 2 * PI *r;
+
+// console.log(circleLength);
+
+// let bottom = prompt('PLease give me your bottom')
+// let test = prompt('Please give me ur top')
+// let height = prompt('Please give me ur height')
+// let trapezoid = (bottom+test)/ 2 * height;
+// // alert (trapezoid);
+
+// let a = prompt('Please give me A number');
+// let b = prompt('Please give me B number');
+
+// if (a < b) {
+//     console.log('it.s true')
+// }
+// else {
+//     console.log('it.s false')
+// }
+
+// birth date exercise bruhh
+
+// const yearOfBirth = prompt('Give me your birth date')
+// const currentYear = 2022;
+
+// const old = currentYear-yearOfBirth;
+
+// if (0 <= old && old <= 1) {
+//     console.log('infant')
+// }
+// else if(1 < old && old <= 3){
+//     console.log('Toddler')
+// }
+// else if(3 < old && old <= 5){
+//     console.log('Preschool')
+// }
+// else if(5 < old && old <= 12){
+//     console.log('Gradeschool')
+// }
+// else if(12 < old && old <= 18){
+//     console.log('Teen')
+// }
+// else if(18 < old && old <= 21){
+//     console.log('Young Adult')
+// }
+// else if(21 < old){
+//     console.log('Adult')
+// }
+
+// const gibMeNumber = prompt ('Heeelloooo Number Plis')
+
+//  if ((gibMeNumber%3)==1) {
+//     console.log('Сондгой')
+//  } else {
+//     console.log('Тэгш')
+//  }
+
+
+// const multiply3Or7 = prompt("Let's check")
+
+// if (multiply3Or7 % 3|| multiply3Or7 % 7){
+//     console.log('no')
+// }
+// else {
+//     console.log('yes')
+// }
+
+// bmi index it has a special formula
+
+// const bmiHeight = prompt ('give me height centimeters')
+// const bmiWeight = prompt ('give me weight kilo')
+// const bmi = bmiWeight/(bmiHeight*bmiHeight);
+
+// if (bmi<18) {
+//     console.log ('under weight')
+// } else if (18 <= bmi && bmi < 25) {
+//     console.log ('normal')
+// }
+// else if (25 <= bmi && bmi < 30) {
+//     console.log ('over weight')
+// }
+// else if (30 <= bmi && bmi < 35) {
+//     console.log ('obesity class 1')
+// }
+// else if (35 <= bmi && bmi < 40) {
+//     console.log ('obesity class 2')
+// }
+// else if (40 < bmi) {
+//     console.log ('extreme obesity')
+// }
+
+// BREAK uildel ashiglah console.log(typeof day)-gevel aldaag shalgah arga 
+
+// let day = prompt ("Put a Day");
+// let dayNumber = Number(day);
+// console.log(dayNumber);
+// console.log(typeof(day));
+// let dayName;
+// switch (dayNumber) {
+//     case 1:
+//         dayName = "Monday";
+//         break;
+//     case 2:
+//         dayName = "Tuesday";
+//         break;
+//     case 3:
+//         dayName = "Wednesday";
+//         break;
+//     case 4:
+//         dayName = "Thursday";
+//         break;
+//     case 5:
+//         dayName = "Friday";
+//         break;
+//     case 6:
+//         dayName = "Saturday";
+//         break;
+//     case 7:
+//         dayName = "Sunday";
+//         break;
+//     default:
+//         dayName = "iNVALID DAY"
+//         break;
+// }
+// console.log(dayName)
+
+console.log ("эерэг сөрөг тоо олох арга болмой")
+
+let n = prompt ("Write number")
+
+if (0 < n) {
+        console.log('эерэг')
+    }
+    else if(0 > n){
+        console.log('сөрөг')
+    }
+
+
+
+
+
+
+
+// if (3 <= old && old <= 5)
+//     console.log('Preschool')
+// if (5 <= old && old <= 12)
+//     console.log('Gradeschool')
+// if (12 <= old && old <= 18)
+//     console.log('Teen')
+// if (18 <= old && old <= 21)
+//     console.log('Young adult')
+// if (21 <= old )
+//     console.log('Adult')
