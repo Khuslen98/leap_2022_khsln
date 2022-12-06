dd41ff4c0a744eeb075ed7ee9c9c3fa4510b7b3b
Khuslenu <khuslenu@MacBook-Pro-3.local>
2022-12-06 22:46:38 +0800

    js function 1st exercise done bruh

---
+++ all exercise /js function/js/main.js
@@ -0,0 +1,13 @@
+// const Tamiraa = function() {};
+
+// const object = {
+//   Khuslen: function() {}
+// };
+//     if(Tamiraa > 0) {
+//         // return Tamiraa.name, Khuslen.name;
+//     }
+
+
+// console.log(Tamiraa.name);
+// console.log(object.Khuslen.name);
+
